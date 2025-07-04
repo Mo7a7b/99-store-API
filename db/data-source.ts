943d417cb07f1ecc95a971deeab30380224b9d32
Mo7a7b <mohabibrahim181@gmail.com>
2025-07-05 01:02:55 +0300

    Fix Bug

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,11 +1,15 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { User } from '../src/users/entities/user.entity';
+import { Product } from '../src/products/entities/product.entity';
+import { Order } from '../src/payments/entities/order.entity';
+import { Payment } from '../src/payments/entities/payments.entity';
 dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['dist/**/**/**/*.entity.js'],
+  entities: [User, Product, Order, Payment],
   migrations: ['dist/db/migrations/*.js'],
 };
 
